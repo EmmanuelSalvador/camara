d3072b9682360942fb714502309cc95d4a917eb0
EmmanuelSalvador <emsacervantesdi@ittepic.edu.mx>
2019-11-26 17:05:19 -0700

    Tomar y recortar imagen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -23,5 +23,33 @@ export class HomePage {
 
     });
   }
+  getImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64, ' + imageData;
+    }, (err) => {
 
+    });
+  }
+  cropImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false,
+      allowEdit: true,
+      targetHeight: 300,
+      targetWidth: 300,
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      this.myphoto = 'data:image/jpeg;base64, ' + imageData;
+    }, (err) => {
+
+    });
+  }
 }
